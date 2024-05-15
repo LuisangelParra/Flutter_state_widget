02dca10b63596d59547817884696d7024cc38460
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2024-05-15 16:57:41 -0500

    Update w1.dart

--- lib/ui/widget/w1.dart
+++ lib/ui/widget/w1.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 
 class W1 extends StatelessWidget {
   const W1({super.key});
-  //color: Theme.of(context).primaryColor,
-  // color: Theme.of(context).colorScheme.inversePrimary,
-  // container margin: const EdgeInsets.all(10),
-  // container padding: const EdgeInsets.all(10),
+  //final Function onAdd;
+  //final double value;
   @override
   Widget build(BuildContext context) {
     return Container(
