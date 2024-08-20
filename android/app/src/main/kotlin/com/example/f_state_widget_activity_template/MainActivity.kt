df20015ddd906e4415d994e26b789a88ecd62569
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2024-08-20 10:06:23 -0500

    update android y web

--- android/app/src/main/kotlin/com/example/f_state_widget_activity_template/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/f_state_widget_activity_template/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.f_state_widget_activity_template
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
